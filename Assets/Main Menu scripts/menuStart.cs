32c65206ba1cbd41c5b78dfbc0f68caa3177e6a5
demetrio2244 <61968267+demetrio2244@users.noreply.github.com>
2021-05-20 12:22:40 +0100

    updates

--- Assets/Main Menu scripts/menuStart.cs
+++ Assets/Main Menu scripts/menuStart.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class menuStart : MonoBehaviour
 {
+    public void start()
+    {
+        if (Camera.main.aspect >= 1.7)
+    Debug.Log("16:9");
+    else if (Camera.main.aspect >= 1.5)
+    Debug.Log("3:2");
+    else
+    Debug.Log("4:3");
+ 
+
+    }
+
+
+
+
+
     public void changemenuscene(string scenename)
     {
         Application.LoadLevel(scenename);
